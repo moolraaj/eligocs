54fba05ec776b67ec09f9f636ce9d2f55867bbed
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-04-26 14:08:03 +0530

    added portfolio post category

--- src/app/portfolio/component/portfolioComponent.jsx
+++ src/app/portfolio/component/portfolioComponent.jsx
@@ -1,36 +1,69 @@
-import React from 'react'
+
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-function PortfolioComponent({data}) {
+import { fetchAllportFolio, fetchAllpostsCategory } from '@/utils/apis/Apis';
+
+function PortfolioComponent() {
+  const [portfolioData, setPortfolioData] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const portfolioResponse = await fetchAllportFolio();
+        const categoriesResponse = await fetchAllpostsCategory();
+
+        const portfolioData = await portfolioResponse;
+        const categoriesData = await categoriesResponse;
+
+        setPortfolioData(portfolioData);
+        setCategories(categoriesData);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-     <div className="portfolio_right_section">
-            {data.map((ele) => (
-                <div className="portfolio" key={ele.id}>
-                    <Link href={`/portfolio/${ele.slug}`} passHref className='portfolio-post'>
-                        <div className="portfolio_image">
-                            <img src={ele.acf.portfolio_image} alt="" srcSet="" />
-                        </div>
-                        <div className="portfolio_flex">
-                            <div className="portfolio_inner_left_section">
-                                <div className="portfolio_title">
-                                    <h4>{ele.acf.portfolio_heading}</h4>
-                                </div>
-                                <div className="portfolio_short_description">
-                                    <p dangerouslySetInnerHTML={{ __html: ele.acf.portfolio_short_description }}></p>
-                                </div>
-                            </div>
-                            <div className="portfolio_inner_right_section">
-                                <div className="portfolio_technology">
-                                    <p dangerouslySetInnerHTML={{ __html: ele.acf.portfolio_technology }}></p>
-                                </div>
-                            </div>
-                        </div>
-                    </Link>
+     
+      <div className="portfolio_right_section">
+        {portfolioData.map((post) => (
+          <div className="portfolio" key={post.id}>
+            <Link href={`/portfolio/${post.slug}`} passHref className='portfolio-post'>
+              <div className="portfolio_image">
+                <img src={post.acf.portfolio_image} alt={post.title.rendered} />
+              </div>
+              <div className="portfolio_flex">
+                <div className="portfolio_inner_left_section">
+                  <div className="portfolio_title">
+                    <h4>{post.acf.portfolio_heading}</h4>
+                  </div>
+                  <div className="portfolio_short_description">
+                  <p dangerouslySetInnerHTML={{ __html: post.acf.portfolio_short_description }}></p>
+                  <p dangerouslySetInnerHTML={{ __html: post.acf.portfolio_technology }}></p>
+                  </div>
                 </div>
-            ))}
-        </div>
+                <div className="portfolio_inner_right_section">
+                  <div className="portfolio_category">
+                    {post.categories && post.categories.length > 0 ? (
+                      <p>{post.categories.map((categoryId) => categories.find(cat => cat.id === categoryId).name).join(', ')}</p>
+                    ) : (
+                      <p>No category</p>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default PortfolioComponent
+export default PortfolioComponent;
+
