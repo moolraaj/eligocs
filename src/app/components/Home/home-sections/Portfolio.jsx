a75cfaee36d78b4daaf7db38f29bb01593e2b379
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-05-06 13:32:25 +0530

    updated css

--- src/app/components/Home/home-sections/Portfolio.jsx
+++ src/app/components/Home/home-sections/Portfolio.jsx
@@ -1,28 +1,53 @@
 'use client'
 
-import React, { useEffect, useRef} from 'react';
-
+import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
-import { Draggable } from 'gsap/dist/Draggable';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 import PortfolioComponent from '@/app/portfolio/component/portfolioComponent';
 
-function PortfolioSection({ ele }) {   
-   
+function PortfolioSection({ ele }) {
+
+    const triggerRef = useRef(null);
+
+    useEffect(() => {
+        const pin = gsap.fromTo('.portfolio_inner', {
+            translateX: 0
+        }, {
+            translateX: '-300vw',
+            ease: 'none',
+            duration: 1,
+            scrollTrigger: {
+                trigger: triggerRef.current,
+                start: 'center center',
+                end: '2000 top',
+                scrub: 0.9,
+                pin: true,
+                pinSpacer: false,
+                pinnedContainer: '#horizontal',
+                invalidateOnRefresh: true,
+                onComplete: function () {
+                    document.querySelector('.pin-spacer').style.display = 'none !important';
+                }
+            }
+        })
+
+        return () => {
+            pin.kill()
+        }
+    }, [])
 
 
     return (
-        <>
-      <section id='horizontal'>
+        <section ref={triggerRef} id='horizontal' style={{ width: '100%', height: '90vh', margin: 'auto', overflow: 'hidden', zIndex: '99999' }}>
             <div className="page_outer portfolio_section_outer scrolling">
                 <div className="page_inner portfolio_section_inner">
                     <div className="portfolio_wrapper">
                         <div className="portfolio_left_section">
                             <h1>{ele.acf.portfolio_heading}</h1>
                         </div>
-                        <div className='scrolling_portfolio_section'>
+                        <div className='scrolling_portfolio_section' >
                             <div className="portfolio_inner">
                                 <PortfolioComponent />
                             </div>
@@ -31,7 +56,6 @@ function PortfolioSection({ ele }) {
                 </div>
             </div>
         </section>
-      </>
     );
 }
 
