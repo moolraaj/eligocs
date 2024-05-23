c9de7d159e151bbac8ce89f5a1db7e831629624b
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-05-23 18:03:05 +0530

    updated css

--- src/app/components/Home/home-sections/Portfolio.jsx
+++ src/app/components/Home/home-sections/Portfolio.jsx
@@ -1,154 +1,55 @@
 
-// 'use client'
-// import React, { useRef, useState } from 'react';
-
-
-// import PortfolioComponent from '@/app/portfolio/component/portfolioComponent';
-
-// function PortfolioSection() {
-
-
-//     const itemRef = useRef(null);
-//     const [mouseDown, setMouseDown] = useState(false);
-//     const [startX, setStartX] = useState(0);
-//     const [scrollLeft, setScrollLeft] = useState(0);
-//     const [zIndex, setZIndex] = useState(0);
-
-//     const handleMouseDown = (e) => {
-//         e.stopPropagation(); // Stop the propagation of the click event
-//         setMouseDown(true);
-//         setStartX(e.pageX - itemRef.current.offsetLeft);
-//         setScrollLeft(itemRef.current.scrollLeft);
-//     }
-    
-
-//     const handleMouseLeave = () => {
-//         setMouseDown(false);
-//         setZIndex(999);
-
-//     }
-
-//     const handleMouseUp = () => {
-//         setMouseDown(false);
-//     }
-
-//     const handleMouseMove = (e) => {
-//         if (!mouseDown) return;
-//         e.preventDefault();
-//         const X = e.pageX - itemRef.current.offsetLeft;
-//         const walk = (X - startX) * 2;
-//         itemRef.current.scrollLeft = scrollLeft - walk;
-//     }
-
-
-//     return (
-//         <>
-
-
-
-//             <div className='page_outer portfolio_section_outer scrolling' >
-//                 <div className='portfolio_wrapper' >
-//                     <div className='portfolio_left_section'>
-//                         <h1>Explore Our Work</h1>
-//                     </div>
-//                     <div className='scrolling_portfolio_section' style={{ zIndex: zIndex }} ref={itemRef}
-//                         onMouseDown={handleMouseDown}
-//                         onMouseLeave={handleMouseLeave}
-//                         onMouseUp={handleMouseUp}
-//                         onMouseMove={handleMouseMove}>
-//                         <div className='portfolio_inner' style={{ zIndex: '-999' }}>
-//                             <PortfolioComponent />
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// }
-
-// export default PortfolioSection;
-
-
-'use client'
 import React, { useRef, useState } from 'react';
 import PortfolioComponent from '@/app/portfolio/component/portfolioComponent';
-import Link from 'next/link';
-import nextClick from '../../../assets/headerAssets/arrow.png'
+import arrow from '../../../assets/headerAssets/arrow.png'
 
 function PortfolioSection() {
     const itemRef = useRef(null);
-    const [mouseDown, setMouseDown] = useState(false);
-    const [startX, setStartX] = useState(0);
     const [scrollLeft, setScrollLeft] = useState(0);
-    const [zIndex, setZIndex] = useState(0);
-    const [firstPortfolioSlug, setFirstPortfolioSlug] = useState(null);
 
-    const handleMouseDown = (e) => {
-        e.stopPropagation();
-        setMouseDown(true);
-        setStartX(e.pageX - itemRef.current.offsetLeft);
-        setScrollLeft(itemRef.current.scrollLeft);
-    }
+    const handleScrollLeft = () => {
+        itemRef.current.scrollLeft -= 350; // Adjust the scrolling speed as needed
+        
+    };
+    
 
-    const handleMouseLeave = () => {
-        setMouseDown(false);
-        setZIndex(999);
-    }
- 
-    const handleMouseUp = () => {
-        setMouseDown(false);
-    }
-
-    const handleMouseMove = (e) => {
-        if (!mouseDown) return;
-        e.preventDefault();
-        const X = e.pageX - itemRef.current.offsetLeft;
-        const walk = (X - startX) * 2;
-        itemRef.current.scrollLeft = scrollLeft - walk;
-    }
+    const handleScrollRight = () => {
+        itemRef.current.scrollLeft += 350; // Adjust the scrolling speed as needed
+       
+    };
 
     return (
-        <>
-            <div className='page_outer portfolio_section_outer scrolling'>
-                <div className='portfolio_wrapper'>
-                    <div className='portfolio_left_section'>
-                        <h1>Explore Our Work</h1>
-                    </div>
-                    <div
-                        className='scrolling_portfolio_section'
-                        style={{ zIndex: zIndex }}
-                        ref={itemRef}
-                        onMouseDown={handleMouseDown}
-                        onMouseLeave={handleMouseLeave}
-                        onMouseUp={handleMouseUp}
-                        onMouseMove={handleMouseMove}
-                    >
-                        <div className='portfolio_inner' style={{ zIndex: '-999' }}>
-                            <PortfolioComponent setFirstPortfolioSlug={setFirstPortfolioSlug} />
-                        </div>
-                    </div>
-                    <div className="portfolion_btn_section">
-                        {firstPortfolioSlug && (
-                            <Link href={`/portfolio/${firstPortfolioSlug}`}>
-                                <button
-                                    style={{
-                                        position: 'absolute',
-                                        right: '10px',
-                                        top: '50%',
-                                        transform: 'translateY(-50%)',
-                                        zIndex: '9999999999999',
-                                        background: 'yellow',
-                                        color: '#fff'
-                                    }}
-                                >
-                                   <img src={nextClick.src} alt="nextClick" />
-                                </button>
-                            </Link>
-                        )}
+        <div className='page_outer portfolio_section_outer scrolling'>
+            <div className='portfolio_wrapper'>
+                <div className='portfolio_left_section'>
+                    <h1>Explore Our Work</h1>
+                </div>
+                <div className='scrolling_portfolio_section' ref={itemRef}>
+                    <div className='portfolio_inner'>
+                        <PortfolioComponent />
                     </div>
                 </div>
+                {/* Left Scroll Button */}
+                <img src={arrow.src} alt="arrow" className="scroll-button left"
+                    onClick={handleScrollLeft}
+                    style={{
+                        top: '50%',
+                        left: '10px',
+                        transform: 'translateY(-50%) rotate(180deg)',
+                    }} />
+               
+                {/* Right Scroll Button */}
+                
+                    <img src={arrow.src} alt="arrow"  className="scroll-button right"
+                    onClick={handleScrollRight}
+                    style={{
+                       
+                        top: '50%',
+                        right: '10px',
+                        transform: 'translateY(-50%)', 
+                    }} />
             </div>
-        </>
+        </div>
     );
 }
 
--- src/app/portfolio/component/portfolioComponent.jsx
+++ src/app/portfolio/component/portfolioComponent.jsx
@@ -1,65 +1,35 @@
-// 'use client'
-// import { allExportedApi } from "@/utils/apis/Apis.jsx";
-// import PortfolioChild from "./portfolioChild";
-// import { useEffect, useState } from "react";
-
-// export default  function PortfolioComponent() {
-//     let api=allExportedApi()
-
-//    const[data,setData]=useState() 
-   
-//    const loadPortfolio=async()=>{
-//     let result=await api.fetchAllportFolio()
-//     setData(result)
-
-//    }
-    
-    
-//     useEffect(()=>{
-//         loadPortfolio()
-//     },[])
-
-  
-
-//     return (
-//        <>
-
-//        <PortfolioChild data={data}/>
-//        </>
-//     );
-// }
-
- 
-
+'use client'
 import { allExportedApi } from "@/utils/apis/Apis.jsx";
 import PortfolioChild from "./portfolioChild";
 import { useEffect, useState } from "react";
 
-export default function PortfolioComponent({ setFirstPortfolioSlug }) {
-    const api = allExportedApi();
-    const [data, setData] = useState([]);
+export default  function PortfolioComponent() {
+    let api=allExportedApi()
 
-    const loadPortfolio = async () => {
-        try {
-            const result = await api.fetchAllportFolio();
-            setData(result);
+   const[data,setData]=useState() 
+   
+   const loadPortfolio=async()=>{
+    let result=await api.fetchAllportFolio()
+    setData(result)
 
-            if (result.length > 0) {
-                // Reversing the setting of the first portfolio slug
-                setFirstPortfolioSlug(result[result.length - 1].slug);
-            }
-        } catch (error) {
-            // Handle errors here
-        }
-    }
+   }
     
-    useEffect(() => {
-        loadPortfolio();
-    }, []);
+    
+    useEffect(()=>{
+        loadPortfolio()
+    },[])
+
+  
 
     return (
-        <>
-            <PortfolioChild data={data} />
-        </>
+       <>
+
+       <PortfolioChild data={data}/>
+       </>
     );
 }
+
+ 
+
+
+
