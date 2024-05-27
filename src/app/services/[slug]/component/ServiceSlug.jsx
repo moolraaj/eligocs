f93c253db70349cc5015f8cfa0a9772cf1901aa0
moolraaj <116911577+moolraaj@users.noreply.github.com>
2024-05-27 13:59:31 +0530

    changes in css

--- src/app/services/[slug]/component/ServiceSlug.jsx
+++ src/app/services/[slug]/component/ServiceSlug.jsx
@@ -9,29 +9,29 @@ import { useEffect, useState } from "react";
 
 
 function ServicesInnerSlug({ slug }) {
-   let api=allExportedApi()
-
-   
-    
-
- 
-
-   let[data,setData]=useState([])
-   let[services,setServices]=useState([])
+   let api = allExportedApi()
 
 
-   const funcLoadSingleSrvices=async()=>{
+
+
+
+
+   let [data, setData] = useState([])
+   let [services, setServices] = useState([])
+
+
+   const funcLoadSingleSrvices = async () => {
       const response = await api.fetchSingleService(slug);
       setData(response)
    }
-   const funcLoadAllSrvices=async()=>{
-      let response=await api.fetchAllServices()
+   const funcLoadAllSrvices = async () => {
+      let response = await api.fetchAllServices()
       setServices(response)
    }
-   useEffect(()=>{
+   useEffect(() => {
       funcLoadSingleSrvices()
       funcLoadAllSrvices()
-   },[])
+   }, [])
 
    return (
       <>
@@ -58,11 +58,11 @@ function ServicesInnerSlug({ slug }) {
                            <h1>{ele.acf.services_inner_heading}</h1>
                         </div>
                         <div className="services_inner_description">
-                           <span dangerouslySetInnerHTML={{__html:ele.acf.services_description}}></span>
+                           <span dangerouslySetInnerHTML={{ __html: ele.acf.services_description }}></span>
                         </div>
                      </div>
                      <div className="services_right_sec">
-                     <h1>Related Services</h1>
+                        <h1>Related Services</h1>
                         <div className="related-services">
                            {
                               relatedServices.map((items) => {
@@ -81,25 +81,26 @@ function ServicesInnerSlug({ slug }) {
                      </div>
                   </div>
 
-                  {ele.acf.inner_development_process.map((ele, index) => (
-                     <div className={`process_flex_template process_template-${index % 2 === 0 ? "even" : "odd"}`}
-                     key={index}>
-                        <div className="process_left_section">
-                           <div className="inner_process_heading">
-                              <h1>{ele.services_process_heading}</h1>
+                  <div className="services_development_process_wrapper">
+                     {ele.acf.inner_development_process.map((ele, index) => (
+                        <div className={`process_flex_template process_template-${index % 2 === 0 ? "even" : "odd"}`}
+                           key={index}>
+                           <div className="process_left_section">
+                              <div className="inner_process_heading">
+                                 <h1>{ele.services_process_heading}</h1>
+                              </div>
+                              <div className="process_description">
+                                 <p dangerouslySetInnerHTML={{ __html: ele.services_process_description }}></p>
+                              </div>
                            </div>
-                           <div className="process_description">
-                              <p dangerouslySetInnerHTML={{ __html: ele.services_process_description }}></p>
+                           <div className="process_right_section">
+                              <div className="process_image">
+                                 <img src={ele.services_process_image} alt='' />
+                              </div>
                            </div>
                         </div>
-                        <div className="process_right_section">
-                           <div className="process_image">
-                              <img src={ele.services_process_image} alt='' />
-                           </div>
-                        </div>
-                     </div>
-                  ))}
-
+                     ))}
+                  </div>
                   <div className="services_dont_miss_out">
                      <div className="services_missout_heading">
                         <h1>{ele.acf.dont_miss_out_heading}</h1>
