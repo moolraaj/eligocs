2b39d2a58988d9a8ccc766e98d5046207ab7d783
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-05-31 19:37:46 +0530

    updated footer pages css

--- src/app/privacy-policy/components/PrivacyPolicy.jsx
+++ src/app/privacy-policy/components/PrivacyPolicy.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function PrivacyPolicy({data}) {
   return (
     <>
-    <div className="privacy_policy_outer page_top">
+    <div className="privacy_policy_outer page_top footer_pages">
         <div className="privacy_policy_inner">
             {data.map((ele,index)=>{
                 return  <div className='privacy_policies_data' key={index} dangerouslySetInnerHTML={{__html: ele.acf.privacy_policies}}></div>;
