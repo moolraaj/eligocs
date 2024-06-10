c2dc1e80f8264f8e5bc66e2dd56c3757c3f011e1
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-06-10 15:32:02 +0530

    updated code

--- src/app/services/[slug]/component/ServiceSlug.jsx
+++ src/app/services/[slug]/component/ServiceSlug.jsx
@@ -88,7 +88,7 @@ function ServicesInnerSlug({ slug }) {
                               <div className={`process_flex_template process_template-${index % 2 === 0 ? "even" : "odd"}`} key={index}>
                                  <div className="process_left_section">
                                     <div className="inner_process_heading">
-                                       <h1>{process.services_process_heading || "Process Heading"}</h1>
+                                       <h1>{process.services_process_heading || ""}</h1>
                                     </div>
                                     <div className="process_description">
                                        <p dangerouslySetInnerHTML={{ __html: process.services_process_description || "" }}></p>
