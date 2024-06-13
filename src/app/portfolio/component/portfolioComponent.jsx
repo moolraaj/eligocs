47f2784ad388627d5539776a8a9655c345345241
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-06-13 15:29:40 +0530

    updated code

--- src/app/portfolio/component/portfolioComponent.jsx
+++ src/app/portfolio/component/portfolioComponent.jsx
@@ -3,33 +3,48 @@ import { allExportedApi } from "@/utils/apis/Apis.jsx";
 import PortfolioChild from "./portfolioChild";
 import { useEffect, useState } from "react";
 
-export default  function PortfolioComponent() {
-    let api=allExportedApi()
+export default function PortfolioComponent() {
+    let api = allExportedApi()
 
-   const[data,setData]=useState() 
-   
-   const loadPortfolio=async()=>{
-    let result=await api.fetchAllportFolio()
-    setData(result)
+    const [data, setData] = useState()
+    const [loading, setLoading] = useState(true)
 
-   }
-    
-    
-    useEffect(()=>{
-        loadPortfolio()
-    },[])
 
-  
+    const loadPortfolio = async () => {
+        try {
+            let result = await api.fetchAllportFolio()
+            setData(result)
+        } catch (error) {
+            console.error("Failed to load services", error)
+        }
+    }
+
+
+    useEffect(() => {
+        const loadData = async () => {
+            await loadPortfolio()
+            setLoading(false)
+        }
+        loadData()
+    }, [])
+
+
 
     return (
-       <>
+        <>
+            {loading ? (
+                <div className="page_top">
+                    <p className="loading_data">Loading...</p>
+                </div>
+            ) : (
+                <PortfolioChild data={data} />
+            )}
 
-       <PortfolioChild data={data}/>
-       </>
+        </>
     );
 }
 
- 
+
 
 
 
